cc910a881813dea63421bac35aace618c8d873ab
seino914 <tonosaki914@icloud.com>
2026-03-01 18:13:20 +0900

    postメソッド追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { Hono } from 'hono';
 import { renderer } from './renderer';
+import { createPostSchema } from './validation_schema/validator';
+import { zValidator } from '@hono/zod-validator';
 
 const app = new Hono<{ Bindings: { DB: D1Database } }>();
 
@@ -17,6 +19,13 @@ app.get('/', async (c) => {
       </ul>
     </>
   );
+});
+
+app.post('/posts', zValidator('form', createPostSchema), async (c) => {
+  const { title, body } = c.req.valid('form');
+  const sql = 'INSERT INTO posts (title, body) VALUES (?, ?)';
+  await c.env.DB.prepare(sql).bind(title, body).run();
+  return c.redirect('/posts');
 });
 
 export default app;
